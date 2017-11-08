0385d51617fd3a60fde09c050d7c9aa9d9dc930f
t_kytsenko <taras_kytsenko@mail.ua>
2017-11-08 22:06:21 +0300

    Post_get2

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,11 +76,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'VrKing99$telegrambot',
-        'USER': 'VrKing99',
-        'PASSWORD': 'werbowe99',
-        'HOST': 'VrKing99.mysql.pythonanywhere-services.com',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
