2025803d837ae6e835a240dea8c63744d4fa0f51
t_kytsenko <taras_kytsenko@mail.ua>
2017-11-09 19:11:25 +0300

    many price options2

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+import MySQLdb
+
+
 
 
 
@@ -15,9 +18,18 @@ def post_list(request):
     return render(request, 'blog/pay.html')"""
 
 def good_pay(request):
+    db = MySQLdb.connect(host='VrKing99.mysql.pythonanywhere-services.com',    # your host, usually localhost
+                 user='VrKing99',         # your username
+                 passwd='werbowe99',  # your password
+                 db='VrKing99$telegrambot',
+                 charset='utf8')        # name of the data base
 
+    cursor2 = db.cursor()
+    sql2="select tel from user_info limit 1;"
+    cursor2.execute(sql2)
+    data_sms =  cursor2.fetchall()
     if 'q' in request.GET:
-        message = 'You searched for: %r ' % (request.GET['q'])
+        message = 'You searched for: %r %s' % (request.GET['q'],data_sms[0][0])
     else:
         message = 'You submitted an empty form.'
     return HttpResponse(message)
